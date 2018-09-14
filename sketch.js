8ec51d0dba0aff349abe568894e841399f56a15f
AurelienThms <43281506+AurelienThms@users.noreply.github.com>
2018-09-14 17:00:42 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,55 +1,62 @@
 let squares = [];
-let scl = window.innerWidth/50;
+let scl = window.innerWidth / 40;
 let angle = 0;
 let lignes = window.innerHeight / scl;
 let collones = window.innerWidth / scl;
+let SmartphoneCheck = false;
 
 function setup() {
-    createCanvas(windowWidth, windowHeight, WEBGL);
-    pointLight(255,255,255,-windowWidth/2,-windowHeight/2,-300);
-    pointLight(255,255,255,windowWidth/2,-windowHeight/2,-300);
-    pointLight(255,255,255,-windowWidth/2,windowHeight/2,-300);
-    pointLight(255,255,255,windowWidth/2,windowHeight/2,-300);
-    pointLight(255,255,255,-windowWidth/2,-windowHeight/2,-300);
-    pointLight(255,255,255,windowWidth/2,-windowHeight/2,-300);
-    pointLight(255,255,255,-windowWidth/2,windowHeight/2,-300);
-    pointLight(255,255,255,windowWidth/2,windowHeight/2,-300);
-    pointLight(255,255,255,0,0,-300);
-
-
-
-
+    createCanvas(window.innerWidth, window.innerHeight, WEBGL);
+    pointLight(255, 255, 255, -windowWidth / 2, -windowHeight / 2, -300);
+    pointLight(255, 255, 255, windowWidth / 2, -windowHeight / 2, -300);
+    pointLight(255, 255, 255, -windowWidth / 2, windowHeight / 2, -300);
+    pointLight(255, 255, 255, windowWidth / 2, windowHeight / 2, -300);
+    pointLight(255, 255, 255, -windowWidth / 2, -windowHeight / 2, -300);
+    pointLight(255, 255, 255, windowWidth / 2, -windowHeight / 2, -300);
+    pointLight(255, 255, 255, -windowWidth / 2, windowHeight / 2, -300);
+    pointLight(255, 255, 255, windowWidth / 2, windowHeight / 2, -300);
+    pointLight(255, 255, 255, 0, 0, -300);
 
 }
 
 function draw() {
-    //ortho();
-    translate(-windowWidth / 2, -windowHeight / 2);
-   fill(255,0,0);
-    background(40,0);
-    angleY =angle;
-    angleX = 2.35339;
-    angle+=0.02;
-    create();
+    if (windowWidth > windowHeight) {
+        //ortho();
+        translate(-windowWidth / 2, -windowHeight / 2);
+        fill(255, 0, 0);
+        background(40, 0);
+        angleY = angle;
+        angleX = 2.35339;
+        angle += 0.01;
+        create();
+    }else {
+        scl = window.innerWidth / 16;
+        translate(-windowWidth / 2, -windowHeight / 2);
+        fill(255, 0, 0);
+        background(40, 0);
+        angleY = angle;
+        angleX = 2.35339;
+        angle += 0.02;
+        create();
+    }
 }
 
 function create() {
-    for (let i = 0; i < lignes+1 ; i++) {
-        for (let x = 0; x < collones+1 ; x++) {
-            specularMaterial(10,10,10);
+    for (let i = 0; i < lignes + 1; i++) {
+        for (let x = 0; x < collones + 1; x++) {
+            specularMaterial(10, 10, 10);
             strokeWeight(2);
             stroke(255);
             noStroke();
             rotateX(angleY);
             rotateY(angleX);
-            box(scl+0.1);
+            box(scl + 0.1);
             rotateY(-angleX);
             rotateX(-angleY);
-            translate(scl*(Math.sqrt(2)), 0);
+            translate(scl * (Math.sqrt(2)), 0);
         }
-        translate(-collones*(scl*(Math.sqrt(2)))-3*(scl*(Math.sqrt(2))/2),scl);
+        translate(-collones * (scl * (Math.sqrt(2))) - 3 * (scl * (Math.sqrt(2)) / 2), scl);
     }
 }
-
 
 
